f2ab5d95f6f949362b2fdae301c0ad5e720ef840
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2019-10-08 11:47:08 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'normalize.css/normalize.css'
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import MainApp from './components/MainApp'
+import './styles/styles.scss'
+
+ReactDOM.render(<MainApp />, document.getElementById('root'))
